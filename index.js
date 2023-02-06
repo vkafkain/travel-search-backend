f061cbd78efc1129363df6be8dcc990f913dda49
Víctor López <vlopezmoles@gmail.com>
2023-02-06 09:58:25 +0100

    index view validators

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ import fakeData from './database/fakeData.js';
 import { connectDB, sequelize } from './database/mySQL.js';
 import readline from 'readline-sync';
 
+const regex = /^[\p{L}]+$/u
 sequelize.options.logging = false;
 
 connectDB().then(() => {
   fakeData()
-}).then(() => {
+}).then(async () => {
   const searchInput = readline.question(`Welcome to seach engine! \n Enter your destination:  `);
+  if(searchInput.length < 3){
+    console.log('Enter at least three letters to start the search!');
+  }
+  if(!searchInput.match(regex)) {
+    console.log('Only letters allowed')
+  }
+  
+
+  // console.log(searchInput);
 }) 
+
+//buscar ciudad por el input introducido
 
 
 

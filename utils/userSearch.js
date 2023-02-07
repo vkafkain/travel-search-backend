7c0c468d927a7963ca5961939d16bf23babf5c9b
Víctor López <vlopezmoles@gmail.com>
2023-02-07 22:27:09 +0100

    minor search moduled

--- utils/userSearch.js
+++ utils/userSearch.js
@@ -1,10 +1,11 @@
 import readline from 'readline-sync';
+import { searchTrips } from '../controllers/search.controller.js';
 
 const regex = /^[\p{L}]+$/u;
 
 const userSearch = () => {
   const searchInput = readline.question(
-    `Welcome to search engine! \n Enter your destination:  `
+    `\n Control + C for Exit\n Welcome to search engine! \n Enter city name:\n `
   );
   if (searchInput.length < 3) {
     console.log('Enter at least three letters to start the search!');
@@ -15,4 +16,10 @@ const userSearch = () => {
   return searchInput;
 };
 
-export default userSearch;
+const startSearch = async () => {
+  const searchInput = userSearch();
+  await searchTrips(searchInput);
+  startSearch();
+}
+
+export default startSearch;

7380ef236296ea79bb33313ab9c32b6a6413ad77
Víctor López <vlopezmoles@gmail.com>
2023-02-07 22:25:40 +0100

    added new data to fake data

--- database/fakeData.js
+++ database/fakeData.js
@@ -4,21 +4,25 @@ import Flight from '../models/Flight.js';
 import City from '../models/City.js';
 import Hotel from '../models/Hotel.js';
 
-export default function fakeData() {
+export default async function fakeData() {
   const city = [
     { name: 'Berlin', country: 'Germany' },
     { name: 'Paris', country: 'France' },
     { name: 'London', country: 'UK' },
     { name: 'Rome', country: 'Italy' },
     { name: 'Madrid', country: 'Spain' },
+    { name: 'Barcelona', country: 'Spain' },
+    { name: 'القاهرة', country: 'مصر' },
+    { name: '上海', country: '中国' },
   ];
+ 
 
   const hotel = [
-    { name: 'The Ritz Carlton', categories: '5 stars' },
-    { name: 'Hilton Paris Opera', categories: '4 stars' },
-    { name: 'The Savoy', categories: '5 stars' },
-    { name: 'Grand Hotel Parco dei Principi', categories: '5 stars' },
-    { name: 'Hotel NH Collection Madrid Eurobuilding', categories: '4 stars' },
+    { name: 'The Ritz Carlton', category: '5 stars' },
+    { name: 'Hilton Paris Opera', category: '4 stars' },
+    { name: 'The Savoy', category: '5 stars' },
+    { name: 'Grand Hotel Parco dei Principi', category: '5 stars' },
+    { name: 'Hotel NH Collection Madrid Eurobuilding', category: '4 stars' },
   ];
 
   const flight = [
@@ -27,18 +31,32 @@ export default function fakeData() {
     { departure_city: 'London', departure_time: '2022-10-03 08:00:00' },
     { departure_city: 'Rome', departure_time: '2022-10-04 07:00:00' },
     { departure_city: 'Madrid', departure_time: '2022-10-05 06:00:00' },
+    { departure_city: 'Barcelona', departure_time: '2022-11-05 06:30:00' },
+    { departure_city: '上海', departure_time: '2022-11-05 06:30:00' },
+    { departure_city: 'القاهرة', departure_time: '2022-11-05 06:30:00' },
   ];
 
   const trip = [
-    { name: 'European Adventure', type: 'land trip', duration: '10:00:00' },
-    { name: 'City Break', type: 'air trip', duration: '05:00:00' },
-    { name: 'Luxury Getaway', type: 'air trip', duration: '07:00:00' },
+    { name: 'European Adventure', type: 'land trip', duration: '5 days' },
+    { name: 'City Break', type: 'air trip', duration: '11 days' },
+    { name: 'Luxury Getaway', type: 'air trip', duration: '8 days' },
+    { name: 'Chinese Adventure', type: 'land trip', duration: '8 days' },
   ];
 
-  sequelize.sync({ force: true }).then(async () => {
-    await City.bulkCreate(city);
-    await Hotel.bulkCreate(hotel);
-    await Flight.bulkCreate(flight);
-    await Trip.bulkCreate(trip);
-  });
-}
+  await sequelize.sync({ force: true })
+    const cities = await City.bulkCreate(city);
+    const hotels = await Hotel.bulkCreate(hotel);
+    const flights = await Flight.bulkCreate(flight);
+    const trips = await Trip.bulkCreate(trip);
+
+    for (const [index, flight] of flights.entries()){
+      await flight.setTrip(trips[index % trips.length]);
+    }
+
+    for (let i = 0; i < trips.length; i++) {
+      await trips[i].setFlights([flights[2 * i], flights[2 * i + 1]]);
+      await trips[i].setCities(cities);
+      await trips[i].setHotels(hotels);
+    }
+  };
+
